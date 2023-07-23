72d1394fbe63ca6e6b27e165f85d55d8b717034f
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-23 17:10:50 +0800

    frames and layout for basic information

--- add_entry.py
+++ add_entry.py
@@ -27,3 +27,32 @@ class AddEntryWindow:
 
         def go_back():
             self.add_entry_toplevel.destroy()
+
+        # Basic Information Frame
+        basic_info_frame = ttk.LabelFrame(self.add_entry_frame, text="Basic Information")
+        basic_info_frame.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+
+        # Labels and Entry widgets
+        label_name = ttk.Label(basic_info_frame, text="Name:")
+        label_phone = ttk.Label(basic_info_frame, text="Phone:")
+        label_address = ttk.Label(basic_info_frame, text="Address:")
+        label_date_visited = ttk.Label(basic_info_frame, text="Date Visited:")
+
+        # Prefix self. to make these instance attributes
+        self.entry_name = ttk.Entry(basic_info_frame)
+        self.entry_phone = ttk.Entry(basic_info_frame)
+        self.entry_address = ttk.Entry(basic_info_frame)
+        self.entry_date_visited = ttk.Entry(basic_info_frame)
+
+        # Grid layout for basic information
+        label_name.grid(row=0, column=0, sticky="w")
+        label_phone.grid(row=1, column=0, sticky="w")
+        label_address.grid(row=2, column=0, sticky="w")
+        label_date_visited.grid(row=3, column=0, sticky="w")
+        self.entry_name.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+        self.entry_phone.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+        self.entry_address.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+        self.entry_date_visited.grid(row=3, column=1, padx=5, pady=5, sticky="w")
+
+        # Add style for basic_info_frame
+        basic_info_frame.configure(style="Accent.TLabelframe")
