b265490a5af98407e121bd03980cc232413040a3
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-23 15:34:51 +0800

    define function to read data

--- csv_operations.py
+++ csv_operations.py
@@ -4,3 +4,11 @@ def write_to_csv(data):
     with open("contact_tracing.csv", "a", newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(data)
+
+def search_csv_by_name(name):
+    with open("contact_tracing.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row[0] == name:
+                return row
+    return None
