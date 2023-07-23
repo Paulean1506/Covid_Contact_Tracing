44f89c67ff9da9cd3f2c9a5a27ea3ec2218d086a
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-23 15:23:11 +0800

    create custom style

---
+++ styles.py
@@ -0,0 +1,6 @@
+import tkinter.ttk as ttk
+
+# Create a custom style
+def configure_custom_style():
+    style = ttk.Style()
+    style.configure("Accent.TLabelframe", foreground="blue", background="white", font=("Helvetica", 12))
