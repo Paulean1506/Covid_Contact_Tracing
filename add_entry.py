1adf38725f3429794656578496b66ef56717cbf3
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-23 21:46:06 +0800

    frames and layout for contact info with checkbox

--- add_entry.py
+++ add_entry.py
@@ -96,3 +96,69 @@ class AddEntryWindow:
         quarantine_loa_checkbox.grid(row=1, column=1, padx=5, pady=5, sticky="w")
         quarantine_shn_checkbox.grid(row=2, column=0, padx=5, pady=5, sticky="w")
         quarantine_none_checkbox.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+
+        # Contact Information Frame
+        contact_info_frame = tk.LabelFrame(self.add_entry_frame, text="Contact Information")
+        contact_info_frame.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+
+        # Checkboxes for contact
+        self.checkbox_contact = tk.IntVar()
+        contact_label = tk.Label(contact_info_frame, text="Have you been in contact with any COVID-19 confirmed infected/diagnosed or suspected person(s), or any person(s) under Quarantine Order (QO) / Leave of Absence (LOA) / Stay-home Notice (SHN)?")
+        contact_yes_checkbox = tk.Checkbutton(contact_info_frame, text="Yes", variable=self.checkbox_contact, onvalue=1, offvalue=0)
+        contact_no_checkbox = tk.Checkbutton(contact_info_frame, text="No", variable=self.checkbox_contact, onvalue=0, offvalue=1)
+
+        # Additional Information for contact
+        contact_info_label = tk.Label(contact_info_frame, text="Stranger / dd-mm-yyyy (If yes, please tell us your relationship with the people and your last contact date with them):")
+        self.contact_info_entry = tk.Entry(contact_info_frame)
+
+        # Grid layout for contact information
+        contact_label.grid(row=0, column=0, columnspan=2, sticky="w")
+        contact_yes_checkbox.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        contact_no_checkbox.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+        contact_info_label.grid(row=2, column=0, columnspan=2, sticky="w")
+        self.contact_info_entry.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky="w")
+
+        # Health Information Frame
+        health_info_frame = tk.LabelFrame(self.add_entry_frame, text="Health Information")
+        health_info_frame.grid(row=4, column=0, padx=10, pady=10, sticky="w")
+
+        # Checkboxes for health
+        self.checkbox_fever = tk.IntVar()
+        self.checkbox_cough = tk.IntVar()
+        self.checkbox_sore_throat = tk.IntVar()
+        self.checkbox_runny_nose = tk.IntVar()
+        self.checkbox_persistent_pain = tk.IntVar()
+        self.checkbox_shortness_breath = tk.IntVar()
+
+        health_label = tk.Label(health_info_frame, text="Please state whether you've experienced/are experiencing the following:")
+
+        health_fever_checkbox = tk.Checkbutton(health_info_frame, text="Fever", variable=self.checkbox_fever, onvalue=1, offvalue=0)
+        health_cough_checkbox = tk.Checkbutton(health_info_frame, text="Cough", variable=self.checkbox_cough, onvalue=1, offvalue=0)
+        health_sore_throat_checkbox = tk.Checkbutton(health_info_frame, text="Sore Throat", variable=self.checkbox_sore_throat, onvalue=1, offvalue=0)
+        health_runny_nose_checkbox = tk.Checkbutton(health_info_frame, text="Runny Nose", variable=self.checkbox_runny_nose, onvalue=1, offvalue=0)
+        health_persistent_pain_checkbox = tk.Checkbutton(health_info_frame, text="Persistent Pain in the Chest", variable=self.checkbox_persistent_pain, onvalue=1, offvalue=0)
+        health_shortness_breath_checkbox = tk.Checkbutton(health_info_frame, text="Shortness of Breath", variable=self.checkbox_shortness_breath, onvalue=1, offvalue=0)
+
+        # Grid layout for health information
+        health_label.grid(row=0, column=0, columnspan=2, sticky="w")
+        health_fever_checkbox.grid(row=1, column=0, sticky="w")
+        health_cough_checkbox.grid(row=2, column=0, sticky="w")
+        health_sore_throat_checkbox.grid(row=3, column=0, sticky="w")
+        health_runny_nose_checkbox.grid(row=4, column=0, sticky="w")
+        health_persistent_pain_checkbox.grid(row=5, column=0, sticky="w")
+        health_shortness_breath_checkbox.grid(row=6, column=0, sticky="w")
+
+        # Working From Home Frame
+        working_from_home_frame = tk.LabelFrame(self.add_entry_frame, text="Working From Home")
+        working_from_home_frame.grid(row=5, column=0, padx=10, pady=10, sticky="w")
+
+        # Checkboxes for working from home
+        self.checkbox_working_from_home = tk.IntVar()
+        working_from_home_label = tk.Label(working_from_home_frame, text="Are you working from home now?")
+        working_from_home_yes_checkbox = tk.Checkbutton(working_from_home_frame, text="Yes", variable=self.checkbox_working_from_home, onvalue=1, offvalue=0)
+        working_from_home_no_checkbox = tk.Checkbutton(working_from_home_frame, text="No", variable=self.checkbox_working_from_home, onvalue=0, offvalue=1)
+
+        # Grid layout for working from home information
+        working_from_home_label.grid(row=0, column=0, columnspan=2, sticky="w")
+        working_from_home_yes_checkbox.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        working_from_home_no_checkbox.grid(row=1, column=1, padx=5, pady=5, sticky="w")
