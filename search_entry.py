5662f264d2fe09226ff82e27a793625191b5794e
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-24 14:24:09 +0800

    added buttons and labels

--- search_entry.py
+++ search_entry.py
@@ -1,13 +1,27 @@
 import tkinter as tk
-import tkinter.ttk as ttk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
+import csv
 from csv_operations import search_csv_by_name
 from styles import configure_custom_style
 
 class SearchEntryWindow:
-    def __init__(self, root):
+    def __init__(self, root, search_name):
         self.root = root
         self.root.title("COVID Contact Tracing App")
         configure_custom_style()
 
-        
+        # Create a custom style
+        style = ttk.Style()
+        style.configure("Accent.TLabelframe", foreground="blue", background="white", font=("times new roman", 1))
+
+        # Labels
+        self.label_search = ttk.Label(root, text="Search by Name:")
+        self.label_search.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+
+        # Entry widget
+        self.entry_search = ttk.Entry(root)
+        self.entry_search.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+
+        # Button
+        self.button_search = ttk.Button(root, text="Search", command=self.search_entry)
+        self.button_search.grid(row=1, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
