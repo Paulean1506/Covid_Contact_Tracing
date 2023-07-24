a18671452fe36994952cd1e51760c9280ba4d8c7
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-24 20:06:40 +0800

    attribute error

--- main.py
+++ main.py
@@ -30,3 +30,17 @@ class ContactTracingApp:
         self.entry_search.grid(row=1, column=1, padx=5, pady=5, sticky="ew")
         self.button_add_entry.grid(row=2, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
         self.button_search.grid(row=3, column=0, columnspan=2, padx=10, pady=5, sticky="ew")
+
+def add_entry_window(self):
+        self.add_entry_toplevel = tk.Toplevel(self.root)  # Use self.add_entry_toplevel
+        self.add_entry_toplevel.title("Add Entry")
+        add_entry_window = AddEntryWindow(self.add_entry_toplevel)
+
+def search_entry(self):
+        search_name = self.entry_search.get()  # Get the value from the entry widget
+        search_entry_window = SearchEntryWindow(self.root, search_name)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ContactTracingApp(root)
+    root.mainloop()
