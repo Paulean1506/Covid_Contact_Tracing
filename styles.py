acf224346e3a3e909ba8614cbc014e58e39be331
Paulean Marguerette Parrish <129674849+Paulean1506@users.noreply.github.com>
2023-07-26 16:24:57 +0800

    adjusted styles file

--- styles.py
+++ styles.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
+# Adding custom styles
 def set_custom_style():
     style = ttk.Style()
     style.configure("Accent.TLabelframe", foreground="blue", background="white", font=("Helvetica", 12))
